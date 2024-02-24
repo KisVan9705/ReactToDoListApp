1bf2d8e0d5c7bffecf9caba1e6010f6a0401f959
KisVan9705 <kvandari9705@gmail.com>
2024-02-23 23:03:05 -0600

    origin

--- ToDoApp/src/Components/ToDoList.jsx
+++ ToDoApp/src/Components/ToDoList.jsx
@@ -18,17 +18,36 @@ function ToDoList(){
         setTasks(tasks.filter((_, i) => i !== index));
     }
 
+    function moveTaskUp(index){
+        if (index > 0){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index-1]] = [updatedTasks[index-1], updatedTasks[index]]
+            setTasks(updatedTasks);
+        }
+    }
+
+    function moveTaskDown(index){
+        if (index < tasks.length-1){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index+1]] = [updatedTasks[index+1], updatedTasks[index]]
+            setTasks(updatedTasks);
+        }
+    }
+    
+
     return(
         <div className="to-do-list">
             <h1>To Do List</h1>
+            <input id="input-field" type="text" placeholder="Enter a task..."/>
+            <button onClick={handleAddTask}>Add</button>
             <ol>
                 {tasks.map((task,index) => <li key = {index}>
                     {task}
                     <button onClick={() => handleRemoveTask(index)}>Delete</button>
+                    <button onClick={() => moveTaskUp(index)}>👆🏻</button>
+                    <button onClick={() => moveTaskDown(index)}>👇🏻</button>
                 </li>)}
             </ol>
-            <input id="input-field" type="text" placeholder="Enter a task..."/>
-            <button onClick={handleAddTask}>Add</button>
         </div>
     );
 }
